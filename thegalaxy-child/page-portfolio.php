1c1dea93e98d8e72762875f685cb334ef7067870
Corey Sizemore <coreysizemore@gmail.com>
2018-04-16 22:29:20 -0700

    v1.0.0
    Moved the galaxy theme.

---
+++ thegalaxy-child/page-portfolio.php
@@ -0,0 +1,17 @@
+<?php
+	
+	get_header();
+	
+	/*
+		@package WordPress
+		@subpackage thegalaxy
+		Template Name: Page - Portfolio
+	*/
+	 
+?>
+
+<?php get_template_part( 'headers/header', 'page' ); ?>
+
+<?php get_template_part( 'mains/main', 'portfolio' ); ?>
+
+<?php get_footer(); ?>
---
+++ thegalaxy-child/page-pricing.php
@@ -0,0 +1,17 @@
+<?php
+	
+	get_header();
+	
+	/*
+		@package WordPress
+		@subpackage thegalaxy
+		Template Name: Page - Pricing
+	*/
+	 
+?>
+
+<?php get_template_part( 'headers/header', 'page' ); ?>
+
+<?php get_template_part( 'mains/main', 'pricing' ); ?>
+
+<?php get_footer(); ?>
